ffe0fa156d9323f90fa6168332fc2ef4a0dfa636
Alena Neumannová <524762@fi.muni.cz>
2023-06-11 17:37:19 +0200

    fix: refactor New Task button

--- frontend/src/components/Task/Task.tsx
+++ frontend/src/components/Task/Task.tsx
@@ -8,37 +8,20 @@ type TaskProps = {
 function Task({ name, done }: TaskProps) {
   let taskIcon;
   let taskStyle;
-  let taskEditIcon;
 
   if (done) {
-    taskIcon = (
-      <img className="task__icon done" src="../../assets/icons/done.svg" alt="Checklist task icon" />
-    );
+    taskIcon = (<img className="task__icon done" src="../../assets/icons/done.svg" alt="Checklist task icon" />);
     taskStyle = 'task__name done';
-    taskEditIcon = (
-      <img className="flex-none" src="../../assets/icons/edit.svg" alt="Edit task icon"/>
-    );
-  } else if (done === false) {
-    taskIcon = (
-      <div className="task__icon todo"></div>
-    );
+  } else {
+    taskIcon = (<div className="task__icon todo"></div>);
     taskStyle = 'task__name todo';
-    taskEditIcon = (
-      <img className="flex-none" src="../../assets/icons/edit.svg" alt="Edit task icon"/>
-    );
-  } else { // done === undefined
-    taskIcon = (
-      <img className="task__icon new" src="../../assets/icons/add_gray.svg" alt="Checklist task icon" />
-    );
-    taskStyle = 'task__name new';
-    taskEditIcon = (<></>);
   }
 
   return (
       <div className="task">
         {taskIcon}
         <h3 className={taskStyle}>{name}</h3>
-        {taskEditIcon}
+        <img className="flex-none" src="../../assets/icons/edit.svg" alt="Edit task icon"/>
       </div>
   );
 }
