294ad4a7f5e356d9633bcb164e18ca9015745ed6
Jakub Krakovský <514050@fi.muni.cz>
2023-06-12 16:48:40 +0200

    feat: basic seed creating Users in database

--- backend/src/seed/seed.ts
+++ backend/src/seed/seed.ts
@@ -1,25 +1,33 @@
+/* IMPORTANT: Do NOT modify this file */
+/* eslint-disable no-console */
 import { PrismaClient } from '@prisma/client';
+import data from './data/user_data.json';
+import type { UserJson } from './types';
 
 const prisma = new PrismaClient();
+// Create user queries
+const userCreateQueries = data.users.map(
+  ({ ...user }: UserJson) => prisma.user.create({
+    data: {
+      ...user,
+    },
+  }),
+);
 
-async function main() {
-  // await prisma.user.create({
-  //   data: {
-  //     userName: 'Jakub',
-  //     email: 'aa@bobis.com',
-  //     hashedPassword: 'dasd',
-  //     salt: 'dasd',
-  //     editedAt: 's',
-  //     avatar: 's',
-  //   },
-  // });
-}
+// create post queries with nested writes (thanks to the Prisma's awesome API)
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async () => {
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+const seed = async () => {
+  console.log(`[${new Date(Date.now()).toISOString()}]: Seeding started`);
+  try {
+    await prisma.$transaction([
+      ...userCreateQueries,
+    ]);
+
+    console.log(`[${new Date(Date.now()).toISOString()}]: Seeding succesful!`);
+  } catch (e) {
+    console.log(e);
+    console.log(`[${new Date(Date.now()).toISOString()}]: Seeding was not successful. Aborting!`);
+  }
+};
+
+seed();
---
+++ backend/src/seed/types.ts
@@ -0,0 +1,14 @@
+/* IMPORTANT: Do NOT modify this file */
+
+/* These are the types of the data from the JSON document */
+export type UserJson = {
+  id: string;
+  userName: string;
+  email: string;
+  createdAt: string;
+  editedAt?: string;
+  deletedAt?: string;
+  hashedPassword: string;
+  salt: string;
+  avatar: string;
+};
