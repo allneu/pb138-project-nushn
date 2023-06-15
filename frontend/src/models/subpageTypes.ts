96f0a43f9cbf227e8dd571feca571dc38c33731d
Petr Cermak <x483005@fi.muni.cz>
2023-06-15 20:12:47 +0200

    feat: Added subpages service

--- frontend/src/models/subpageTypes.ts
+++ frontend/src/models/subpageTypes.ts
@@ -1,8 +1,42 @@
 export interface SubpageType {
   id: string,
   name: string,
-  creatorID: string,
   description: string,
   icon: string,
-  createdAt: string,
+  creator: {
+    id: string,
+    username: string,
+  },
+  createdAt: Date,
+}
+
+export interface SubpageCreateType {
+  name: string,
+  description: string,
+  icon: string,
+}
+
+export interface SubpageCreateResultType extends SubpageType {
+  labels: [{
+    id: string,
+    name: string,
+    orderInSubpabe: number,
+    createdAt: Date,
+  }],
+}
+
+export interface SubpageUpdateType {
+  oldName?: string,
+  oldDescription?: string,
+  oldIcon?: string,
+  newName?: string,
+  newDescription?: string,
+  newIcon?: string,
+}
+
+export interface SubpageUpdateResultType {
+  id: string,
+  name?: string,
+  description?: string,
+  icon?: string,
 }
