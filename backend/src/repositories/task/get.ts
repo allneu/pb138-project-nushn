e1fe9a6eae75517abfb48195b85c3a322948ed55
Jakub Semeniuk <xsemen@fi.muni.cz>
2023-06-18 14:07:17 +0200

    feat: getMultiple tasks

--- backend/src/repositories/task/get.ts
+++ backend/src/repositories/task/get.ts
@@ -53,42 +53,30 @@ Promise<Result<TaskGetMultipleResult>> => {
         return Result.err(subPageExists.error);
       }
 
-      const labels = await tx.label.findMany({
+      const tasks = (await tx.label.findMany({
         where: { subPageId: data.subpageId, deletedAt: null },
-      });
-
-      const labelTasksPromises = labels.map(async (label) => {
-        const subpageTasks = await tx.task.findMany({
-          where: { labelId: label.id, deletedAt: null },
-          select: {
-            id: true,
-            taskName: true,
-            dueDate: true,
-            content: true,
-            creatorId: true,
-            labelId: true,
-            orderInLabel: true,
-            orderInList: true,
+        select: {
+          tasks: {
+            where: { deletedAt: null },
+            select: {
+              id: true,
+              taskName: true,
+              dueDate: true,
+              content: true,
+              labelId: true,
+              orderInList: true,
+              orderInLabel: true,
+              creator: {
+                select: {
+                  id: true,
+                  userName: true,
+                },
+              },
+            },
           },
-        });
-        return subpageTasks;
-      });
-
-      // there fix the creator getting, idk how
-      const tasks = await Promise.all(labelTasksPromises).then((results) => results.flat());
-      const result: TaskGetMultipleResult = {
-        tasks: tasks.map((task) => ({
-          id: task.id,
-          taskName: task.taskName,
-          dueDate: task.dueDate,
-          content: task.content,
-          creator: { id: task.creatorId, userName: '' }, // Add a dummy userName or fetch it from the user table
-          labelId: task.labelId,
-          orderInLabel: task.orderInLabel!,
-          orderInList: task.orderInList!,
-        })),
-      };
-      return Result.ok(result);
+        },
+      })).map((label) => label.tasks).flat();
+      return Result.ok({ tasks });
     });
   } catch {
     return Result.err(new Error('There was a problem getting labels'));
