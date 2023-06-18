6d85e01110c442bb78490aed662cc72ca56db4a1
Jakub Krakovský <514050@fi.muni.cz>
2023-06-18 11:36:53 +0200

    fix: fixed import

--- backend/src/controllers/user/delete.ts
+++ backend/src/controllers/user/delete.ts
@@ -1,6 +1,5 @@
 import type { Request, Response } from 'express';
 import handleErrors from '../common/handleErrors';
-import { functionalityNotImplemented } from '../common/notimplemented';
 import { userIdSchema } from '../../models/urlParamsSchema';
 import UserRepos from '../../repositories/user';
 import { handleErrResp, handleOkResp } from '../common/handleResponse';
@@ -18,8 +17,6 @@ const deleteUser = async (req: Request, res: Response) => {
     return response.isOk
       ? handleOkResp(204, response.value, res, `Deleted user with id: ${params.userId}.`)
       : handleErrResp(500, response.error, res, response.error.message);
-
-    return await functionalityNotImplemented(req, res);
   } catch (e) {
     return handleErrors(e, res);
   }
