aed278bfff9cb8953c4168dd4846f5029096dc86
Jakub Semeniuk <xsemen@fi.muni.cz>
2023-06-18 18:59:27 +0200

    refactor: createTask

---
+++ backend/src/repositories/common/taks.ts
@@ -0,0 +1,90 @@
+import { SubpageIdType, TaskCreateType } from '../../models';
+import { PrismaTransactionHandle } from './types';
+
+const taskSelect = {
+  id: true,
+  taskName: true,
+  dueDate: true,
+  content: true,
+  labelId: true,
+  orderInList: true,
+  orderInLabel: true,
+  createdAt: true,
+  creator: {
+    select: {
+      id: true,
+      username: true,
+    },
+  },
+};
+
+const labelTaskSelect = {
+  tasks: {
+    where: { deletedAt: null },
+    select: taskSelect,
+  },
+};
+
+const labelsWithTaskSelect = {
+  labels: {
+    where: { deletedAt: null },
+    select: labelTaskSelect,
+  },
+};
+
+export const getSubpageTasks = async (
+  tx: PrismaTransactionHandle,
+  { subpageId }: SubpageIdType,
+) => {
+  const tasks = (await tx.label.findMany({
+    where: { subPageId: subpageId, deletedAt: null },
+    select: labelTaskSelect,
+  })).flatMap((label) => label.tasks);
+  return tasks;
+};
+
+export const getSubpageTasksByLabelId = async (
+  tx: PrismaTransactionHandle,
+  { labelId }: { labelId: string },
+) => {
+  const tasks = (await tx.label.findFirstOrThrow({
+    where: { id: labelId, deletedAt: null },
+    select: {
+      subPage: {
+        select: labelsWithTaskSelect,
+      },
+    },
+  })).subPage.labels.flatMap((label) => label.tasks);
+  return tasks;
+};
+
+export const getHighestLabelOrder = async (
+  { labelId }: TaskCreateType,
+  tx: PrismaTransactionHandle,
+) => {
+  const res = await tx.task.findFirstOrThrow({
+    where: { orderInLabel: { not: null }, labelId },
+    orderBy: {
+      orderInLabel: 'desc',
+    },
+    select: { orderInLabel: true },
+  });
+  return res.orderInLabel;
+};
+
+export const getHighestListOrder = async (
+  { labelId }: TaskCreateType,
+  tx: PrismaTransactionHandle,
+) => {
+  const res = (await getSubpageTasksByLabelId(tx, { labelId }))
+    .reduce((resTask, task) => {
+      if (!resTask.orderInList) {
+        return task;
+      }
+      if (!task.orderInList) {
+        return resTask;
+      }
+      return resTask.orderInList > task.orderInList ? resTask : task;
+    });
+  return res.orderInList;
+};
--- backend/src/repositories/task/create.ts
+++ backend/src/repositories/task/create.ts
@@ -2,6 +2,7 @@ import { Result } from '@badrap/result';
 import { checkLabel } from '../common/common';
 import client from '../client';
 import { Task, TaskCreateType } from '../../models';
+import { getHighestLabelOrder, getHighestListOrder } from '../common/taks';
 
 const create = async (
   data: TaskCreateType,
@@ -12,52 +13,19 @@ const create = async (
       if (labelExists.isErr) {
         return Result.err(labelExists.error);
       }
-      const highestLabelOrder = await tx.task.findFirstOrThrow({
-        where: { orderInLabel: { not: null }, labelId: data.labelId },
-        orderBy: {
-          orderInLabel: 'desc',
-        },
-        take: 1,
-      });
-      const highestListOrder = await tx.task.findFirstOrThrow({
-        where: { orderInList: { not: null } },
-        orderBy: {
-          orderInList: 'desc',
-        },
-        take: 1,
-      });
+      const highestLabelOrder = await getHighestLabelOrder(data, tx);
+      const highestListOrder = await getHighestListOrder(data, tx);
       const newTask = await tx.task.create({
         data: {
-          taskName: data.taskName,
-          dueDate: data.dueDate,
-          content: data.content ? data.content : '',
-          creatorId: data.creatorId,
-          labelId: data.labelId,
-          orderInLabel: highestLabelOrder.orderInLabel ? highestLabelOrder.orderInLabel + 1 : 0,
-          orderInList: highestListOrder.orderInList ? highestListOrder.orderInList + 1 : 0,
-          image: data.image ? data.image : null,
+          ...data,
+          orderInLabel: highestLabelOrder ? highestLabelOrder + 1 : 0,
+          orderInList: highestListOrder ? highestListOrder + 1 : 0,
+        },
+        include: {
+          creator: true,
         },
       });
-      const creator: { id: string, username: string } = await tx.user.findUniqueOrThrow({
-        where: { id: newTask.creatorId },
-        select: { id: true, username: true },
-      });
-      if (newTask.orderInLabel === null || newTask.orderInList == null) {
-        throw new Error('Order error');
-      }
-      const result: Task = {
-        id: newTask.id,
-        taskName: newTask.taskName,
-        dueDate: newTask.dueDate,
-        content: newTask.content,
-        creator,
-        labelId: newTask.labelId,
-        orderInLabel: newTask.orderInLabel,
-        orderInList: newTask.orderInList,
-        createdAt: newTask.createdAt,
-      };
-
-      return Result.ok(result);
+      return Result.ok(newTask);
     });
   } catch (e) {
     return Result.err(new Error('There was a problem in label creation'));
