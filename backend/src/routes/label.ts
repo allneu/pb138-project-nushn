d531646c9d1ec3003da5a8301d2f8c4d379b5c2b
Jakub Semeniuk <xsemen@fi.muni.cz>
2023-06-19 16:43:27 +0200

    fix: labels -> label

--- backend/src/routes/label.ts
+++ backend/src/routes/label.ts
@@ -7,7 +7,7 @@ const labelRouteGeneric = '/subpage/:subpageId/label';
 const labelRouteSpecific = `${labelRouteGeneric}/:labelId`;
 
 // Get subpage Labels
-// GET /subpage/{subpageId}/labels
+// GET /subpage/{subpageId}/label
 labelRouter.get(labelRouteGeneric, LabelController.get);
 
 // Create Label
