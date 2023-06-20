eed26703bcb4f881093e5b3bd808679a2043f089
Petr Cermak <x483005@fi.muni.cz>
2023-06-20 18:45:31 +0200

    fix: axios cookies another fix

--- frontend/src/services/base.ts
+++ frontend/src/services/base.ts
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:3000',
+  headers: {
+    'Access-Control-Allow-Credentials': true,
+  },
   withCredentials: true,
 });
 
