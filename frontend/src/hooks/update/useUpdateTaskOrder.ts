caf2745f34167667ee5c33c467cfb010e300a40b
Petr Cermak <x483005@fi.muni.cz>
2023-06-21 22:12:12 +0200

    fix: fixed task order update

--- frontend/src/hooks/update/useUpdateTaskOrder.ts
+++ frontend/src/hooks/update/useUpdateTaskOrder.ts
@@ -1,15 +1,20 @@
 import { useParams } from 'react-router-dom';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { TasksApi } from '../../services';
-import { TaskUpdateSendType } from '../../models';
+import { TaskUpdateOrderType } from '../../models';
 
 const useUpdateTaskOrder = () => {
   const { subpageId } = useParams();
   const queryClient = useQueryClient();
 
+  const transformData = (data: TaskUpdateOrderType) => {
+    const { taskId, ...rest } = data;
+    return rest;
+  };
+
   const updateTaskFn = (
     data: TaskUpdateOrderType,
-  ) => TasksApi.updateSingle(subpageId!, data.taskId, data);
+  ) => TasksApi.updateSingle(subpageId!, data.taskId, transformData(data));
 
   const { mutateAsync: updateTaskOrder } = useMutation({
     mutationFn: updateTaskFn,
