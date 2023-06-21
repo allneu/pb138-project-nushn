243dbf79f34e7b456e18358e0b8ff4a2a4b94a4b
Jakub Semeniuk <xsemen@fi.muni.cz>
2023-06-21 12:39:49 +0200

    refactor: label delte

--- backend/src/repositories/label/delete.ts
+++ backend/src/repositories/label/delete.ts
@@ -1,46 +1,69 @@
 import { Result } from '@badrap/result';
 import { LabelIdSubpageIdType } from '../../models/urlParamsSchema';
-import { checkSubpage, checkLabel } from '../common/common';
 import client from '../client';
 import { LabelDeleteResult } from '../../models/labelModels';
+import {
+  labelDoesNotExistError, labelWasDeletedError, serverInternalError, wrongSubpageIdError,
+} from '../../models';
+import logger from '../../log/log';
 
 const deleteLabel = async (
-  data: LabelIdSubpageIdType,
+  { labelId, subpageId }: LabelIdSubpageIdType,
 ): Promise<Result<LabelDeleteResult>> => {
+  logger.debug({ label: { delete: 'start' } });
   try {
     return await client.$transaction(async (tx) => {
-      const subPageExists = await checkSubpage(data.subpageId, tx);
-      if (subPageExists.isErr) {
-        return Result.err(subPageExists.error);
-      }
-      const labelExists = await checkLabel(data.labelId, tx);
-      if (labelExists.isErr) {
-        return Result.err(labelExists.error);
-      }
       const deletedAt = new Date();
-      // get curr order
-      const currOrder = await tx.label.findFirstOrThrow({
-        where: { id: data.labelId },
-        select: { orderInSubpage: true, id: true },
+
+      const labelData = await tx.label.findUnique({
+        where: { id: labelId },
+        select: { orderInSubpage: true, deletedAt: true, subPageId: true },
       });
-      // delete the label and set order to null
-      await tx.label.update({
-        where: { id: data.labelId },
-        data: { deletedAt, orderInSubpage: null },
-      });
-      await tx.label.updateMany({
-        where: {
-          orderInSubpage: {
-            not: null,
-            gt: currOrder.orderInSubpage ? currOrder.orderInSubpage : 0,
+      if (!labelData) {
+        throw labelDoesNotExistError;
+      } if (labelData.deletedAt) {
+        throw labelWasDeletedError;
+      } if (subpageId !== labelData.subPageId) {
+        throw wrongSubpageIdError;
+      } if (!labelData.orderInSubpage && labelData.orderInSubpage !== 0) {
+        throw serverInternalError;
+      }
+
+      await (
+        tx.subPage.update({
+          where: { id: subpageId },
+          data: {
+            labels: {
+              updateMany: {
+                where: { orderInSubpage: { gt: labelData.orderInSubpage } },
+                data: { orderInSubpage: { decrement: 1 } },
+              },
+            },
           },
-        },
-        data: { orderInSubpage: { increment: -1 } },
-      });
-      return Result.ok({ labelId: currOrder.id });
+        }),
+        tx.label.update({
+          where: { id: labelId },
+          data: {
+            deletedAt,
+            orderInSubpage: null,
+            tasks: {
+              updateMany: {
+                where: { deletedAt: null },
+                data: { deletedAt },
+              },
+            },
+          },
+          select: {
+            id: true,
+          },
+        })
+      );
+      logger.debug({ label: { delete: 'susseful done' } });
+      return Result.ok({ labelId });
     });
-  } catch {
-    return Result.err(new Error('There was a problem deleting task'));
+  } catch (e) {
+    logger.debug({ label: { delete: 'error' } });
+    return Result.err(e as Error);
   }
 };
 
